5b590db4c0f55f5be6e43a169be13af3a8ba51d7
containedreality <containedreality@users.noreply.github.com>
2024-02-02 12:07:02 -0500

    added fish to the list of commands to be installed

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-packages="jq xclip ruby nnn curl ctags vim git arch-wiki-docs man-db man-pages base-devel arch-install-scripts"
+packages="jq fish xclip ruby nnn curl ctags vim git arch-wiki-docs man-db man-pages base-devel arch-install-scripts"
 
 if [ "$1" = 'install-programs' ]; then
 	sudo pacman -S $packages
+	chsh -s $(which fish)
 fi
 
+mkdir -p ~/.config/fish
 mkdir -p ~/.config/kitty
 cp gitconfig ~/.gitconfig
 cp kitty.conf ~/.config/kitty
 cp bashrc ~/.bashrc
 cp vimrc ~/.vimrc
+cp config.fish ~/.config/fish/config.fish
