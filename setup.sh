79439849aa192f431d9454c8b79ad365b49ebfa0
containedreality <containedreality@users.noreply.github.com>
2024-03-03 14:53:44 -0500

    Added newsboat and modified busybox package to be installed.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-packages="kakoune fish xclip ruby-dev curl busybox git build-essential"
+packages="kakoune newsboat fish xclip ruby-dev curl busybox-static git build-essential"
 
 if [ "$1" = 'install-programs' ]; then
     sudo apt install -y $packages
@@ -8,6 +8,8 @@ fi
 mkdir -p ~/.config/fish
 mkdir -p ~/.config/kitty
 mkdir -p ~/.config/kak/
+mkdir -p ~/.newsboat
+cp newsfeed ~/.newsboat/urls
 cp gitconfig ~/.gitconfig
 cp kitty.conf ~/.config/kitty
 cp config.fish ~/.config/fish/config.fish
