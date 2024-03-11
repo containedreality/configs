334f59767875ef035b7ca79f2c0a1be6bde27551
containedreality <containedreality@users.noreply.github.com>
2024-03-11 01:53:26 -0400

    Changed prompt (again) but to another prompt, see commit for info

--- config.fish
+++ config.fish
@@ -1,9 +1,61 @@
 function fish_greeting
 end
 
-function fish_prompt
-    echo (set_color -o green)$USER@(prompt_hostname)(set_color normal):(set_color -o blue)(prompt_pwd)(set_color normal)'$ '
+# taken from: https://github.com/oh-my-fish/theme-bira/blob/master/functions/fish_prompt.fish, then modified
+# this portion until "END OF PROMPT" is subject to https://github.com/oh-my-fish/theme-bira/blob/master/LICENSE
+function __user_host
+  set -l content 
+  if [ (id -u) = "0" ];
+    echo -n (set_color --bold red)
+  else
+    echo -n (set_color --bold green)
+  end
+  echo -n $USER@(hostname|cut -d . -f 1) (set color normal)
 end
+
+function __current_path
+  echo -n (set_color --bold blue) (pwd) (set_color normal) 
+end
+
+function _git_branch_name
+  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _git_is_dirty
+  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
+end
+
+function __git_status
+  if [ (_git_branch_name) ]
+    set -l git_branch (_git_branch_name)
+
+    if [ (_git_is_dirty) ]
+      set git_info '<'$git_branch"*"'>'
+    else
+      set git_info '<'$git_branch'>'
+    end
+
+    echo -n (set_color yellow) $git_info (set_color normal) 
+  end
+end
+
+function fish_prompt
+  echo -n (set_color white)"╭─"(set_color normal)
+  __user_host
+  __current_path
+  __git_status
+  echo -e ''
+  echo (set_color white)"╰─"(set_color --bold white)"\$ "(set_color normal)
+end
+
+function fish_right_prompt
+  set -l st $status
+
+  if [ $st != 0 ];
+    echo (set_color red) ↵ $st(set_color normal)
+  end
+end
+# END OF PROMPT
 
 fish_add_path -aP "/usr/sbin"
 fish_add_path -aP "/usr/local/sbin"
